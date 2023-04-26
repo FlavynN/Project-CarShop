07196a649b1e3f04269ff3b183842ac7988941c6
flavynn <flavenhow18@gmail.com>
2023-04-26 21:51:39 +0200

    Fixing CarODM and Car Domain

--- src/Models/CarODM.ts
+++ src/Models/CarODM.ts
@@ -7,11 +7,10 @@ export default class CarODM {
 
   constructor() {
     this._schema = new Schema<ICar>({
-      id: { type: String, required: true },
       model: { type: String, required: true },
       year: { type: Number, required: true },
       color: { type: String, required: true },
-      status: { tpye: Boolean, required: true, default: false },
+      status: { type: Boolean, default: false },
       buyValue: { type: Number, required: true },
       doorsQty: { type: Number, required: true },
       seatsQty: { type: Number, requred: true },
